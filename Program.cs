37686802540f9d3fd08c1d1191d2b652d8b0e039
Lime <demn2001@gmail.com>
2022-12-02 04:03:08 +0300

    Copy SimpleTerminalConsole

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
 ﻿public static class Program
 {
-    public static void Main(string[] args)
+    public static void Log(string text)
     {
 
+    }
+
+    public static void Main(string[] args)
+    {
+        Thread thread = new Thread(() =>
+        {
+            while (true)
+            {
+                Log("Current tick: " + System.DateTime.Now.Ticks);
+                Thread.Sleep(100);
+            }
+        });
+        while (true)
+        {
+            Console.ReadKey();
+        }
     }
 }
---
+++ Terminal/SimpleTerminalConsole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTerminal.Terminal
+{
+    public abstract class SimpleTerminalConsole
+    {
+        protected abstract bool IsRunning { get; }
+        protected abstract void RunCommand(string var1);
+        protected abstract void Shutdown();
+
+        protected void ProcessInput(string input)
+        {
+            string command = input.Trim();
+            if (!string.IsNullOrEmpty(command)) RunCommand(command);
+        }
+
+        public void Start() => ReadCommands(Console.In);
+        private void ReadCommands(TextReader reader)
+        {
+            string? line;
+            while (IsRunning && (line = reader.ReadLine()) != null)
+                ProcessInput(line);
+        }
+    }
+}
