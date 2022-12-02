7f3f30fafb29578bd41b87d8740600c4346274e6
Lime <demn2001@gmail.com>
2022-12-02 03:01:52 +0300

    Append example of console (by garry's http://garry.tv/2014/04/23/unity-batchmode-console/)

---
+++ Info/CommandModule.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class CommandModule
+{
+    private Dictionary<string, MethodInfo> _commands = new Dictionary<string, MethodInfo>();
+
+    public CommandModule() { }
+
+    public void GetCommands()
+    {
+         var methods = AppDomain.CurrentDomain.GetAssemblies() 
+        .SelectMany(x => x.GetTypes()) 
+        .Where(x => x.IsClass) 
+        .SelectMany(x => x.GetMethods()) 
+        .Where(x => x.GetCustomAttributes(typeof(Cooper.ConsoleCommand), false).FirstOrDefault() != null);
+
+        foreach (var item in methods)
+        {
+            var parameters = item.GetParameters();
+
+            if(item.ReturnType != typeof(void))
+            {
+                Debug.LogWarning("CONSOLE COMMANDS | Skipping command with prefix '" + item.Name + "' because it has incorrect return type!\n" +
+    " Please make sure your command's return type is void!");
+                continue;
+            }
+
+            var attribute = (Cooper.ConsoleCommand)item.GetCustomAttribute(typeof(Cooper.ConsoleCommand));
+
+            if (attribute.GetPrefix().Contains(" "))
+            {
+                Debug.LogWarning("CONSOLE COMMANDS | Skipping command with prefix '" + item.Name + "' because its prefix contains spaces!\n");
+                continue;
+            }
+
+            if (attribute != null)
+            {
+                _commands.Add(attribute.GetPrefix(), item);
+            }
+        }
+
+        Debug.Log("CONSOLE COMMANDS | Loaded " + _commands.Count + " commands!");
+    }
+
+    public void ExecuteCommand(string parsedPrefix, string[] args)
+    {
+        if(_commands.TryGetValue(parsedPrefix, out MethodInfo method))
+        {
+            try
+            {
+                var requiredMethodParams = method.GetParameters();
+                object[] parameters = new object[requiredMethodParams.Length];
+
+                for (int i = 0; i < requiredMethodParams.Length; i++)
+                {
+                    var para = requiredMethodParams[i];
+                    var type = para.ParameterType;
+
+                    parameters[i] = Convert.ChangeType(args[i], type);
+                }
+
+                var obj = Activator.CreateInstance(method.DeclaringType);
+
+                method.Invoke(obj, parameters);
+                Debug.Log($"CONSOLE COMMANDS | Command executed successfully!");
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("CONSOLE COMMANDS | Command execution failed! Stacktrace: " + e);
+            }
+        }
+        else
+        {
+            Debug.LogError("CONSOLE COMMANDS | Unknown command!");
+        }
+    }
+}
+
+namespace Cooper
+{
+    public class ConsoleCommand : Attribute
+    {
+        private string prefix;
+        public string GetPrefix() => prefix;
+
+        public ConsoleCommand(string prefix) => this.prefix = prefix;
+    }
+}
---
+++ Info/DefaultCommands.cs
@@ -0,0 +1,64 @@
+﻿/// <summary>
+/// Example commands.
+/// If a command fails, you should throw an exception.
+/// To make a new command, just write a method like normal and attach the attribute.
+/// </summary>
+public class DefaultCommands
+{
+    [Cooper.ConsoleCommand("set-port")]
+    public void SetPortCommand(ushort serverPort)
+    {
+        var transport = Mirror.NetworkManager.singleton.GetComponent<Mirror.Transport>();
+
+        if (Mirror.NetworkManager.singleton.mode != Mirror.NetworkManagerMode.Offline)
+            throw new System.Exception("Cannot set port because server is running!");
+
+        if (transport == null) throw new System.Exception("Couldn't find a transport!");
+
+        if (transport is kcp2k.KcpTransport kcp)
+        {
+            kcp.Port = serverPort;
+            return;
+        }
+
+        if (transport is Mirror.SimpleWeb.SimpleWebTransport swt)
+        {
+            swt.port = serverPort;
+            return;
+        }
+
+        if (transport is Mirror.TelepathyTransport tele)
+        {
+            tele.port = serverPort;
+            return;
+        }
+
+        throw new System.Exception("This command is not compatible with your current transport!");
+    }
+
+    [Cooper.ConsoleCommand("stop")]
+    public void ShutdownServer()
+    {
+        var man = Mirror.NetworkManager.singleton;
+
+        if (man.mode == Mirror.NetworkManagerMode.ServerOnly)
+        {
+            man.StopServer();
+        }
+        else
+            throw new System.Exception("Cannot stop server because it has not been started!");
+    }
+
+    [Cooper.ConsoleCommand("start")]
+    public void StartServer()
+    {
+        var man = Mirror.NetworkManager.singleton;
+
+        if (man.mode == Mirror.NetworkManagerMode.Offline)
+        {
+            man.StartServer();
+        }
+        else
+            throw new System.Exception("Cannot start server because it is already started!");
+    }
+}
